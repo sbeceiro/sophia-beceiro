1a2f8c799139b5b79b51cfd4131a6fe432e7f9f9
Sophia Beceiro <sophiabeceiro@Sophias-MacBook-Pro.local>
2020-05-06 20:34:58 -0400

    studio final food tab working

--- studiofinal/main.js
+++ studiofinal/main.js
@@ -1,115 +1,81 @@
-
-
-var objects = [
-	// {
-	// 	"name": "birthday song",
-	// 	"type": "video",
-	// 	"link": "media/birthday.mp4",
-	// 	"tag": "zoom, birthday"
-	// },
-	{
-		"name": "postcard",
-		"type": "image",
-		"link": "media/WriteYrFriends.jpg",
-		"tag": "text"
-	},
-	{
-		"name": "birthday",
-		"type": "image",
-		"link": "media/birthday.jpg",
-		"tag": "zoom, birthday"
+var foods = [ 
+			{
+		"name": "ramen",
+		"title": "image",
+		"link": "<img src= 'media/ramen.jpg'>",
+		"description": "<br>late night snack."
 	},
 	{
 		"name": "chefs",
 		"type": "image",
-		"link": "media/Gnocchi1.jpg",
-		"tag": "food"
+		"link": "<img src= 'media/Gnocchi1.jpg'>",
+		"description": "<br>marissa found a recipe for gnocchi on tasty and she really wanted to try it out. we didn't have a potato masher, but we tried"
 	},
 	{
 		"name": "gnocchi",
 		"type": "image",
-		"link": "media/Gnocchi2.jpg",
-		"tag": "food"
+		"link": "<img src= 'media/Gnocchi2.jpg'>",
+		"description": "<br>i would say this gnocchi was definitely a success."
 	},
 	{
-		"name": "ramen",
+		"name": "penne alla vodka",
 		"type": "image",
-		"link": "media/ramen.jpg",
-		"tag": "food"
+		"link": "<img src= 'media/penne.jpg'>",
+		"description": "<br>this was my first attempt at ever making a sauce from scratch. all we had was this leftover smirnof someone had left in my apartment one weekend pre-corona. this also took twice as long as it should have to prepare becase i couldn't figure out how to use my roommate's blender..."
 	},
 	{
-		"name": "going crazy",
+		"name": "picadillo",
 		"type": "image",
-		"link": "media/goingcrazy.jpg",
-		"tag": "marissa"
+		"link": "<img src= 'media/picadillo.jpg'>",
+		"description": "<br>picadillo is a cuban dish that consitst of ground beef, tomatoes, rasins and olives. the word picadillo roughly translates to minced, or chopping up in small pieces. i tried my hand at this traditional dish using pieces of a recipe that i had from memory and a few google searches. i'd say for my first attempt making this, i was pretty successful, and definitely nostalgic."
 	},
 	{
-		"name": "moon kissed live",
+		"name": "avocado toast",
 		"type": "image",
-		"link": "media/MoonKissed1.png",
-		"tag": "instagram, live, social media"
+		"link": "<img src= 'media/avotoast.jpg'>",
+		"description": "<br>avocado toast topped with a fried egg and bacon."
 	},
 	{
-		"name": "moon kissed live",
+		"name": "pizza",
 		"type": "image",
-		"link": "media/MoonKissed2.jpg",
-		"tag": "art"
+		"link": "<img src= 'media/pizza.jpg'>",
+		"description": "<br>a very simple pizza: cauliflower crust topped with raos tomato sauce and shredded mozzarella."
 	},
 	{
-		"name": "outdoor time",
+		"name": "mac",
 		"type": "image",
-		"link": "media/RoofPicnic2.jpg",
-		"tag": "roof"
+		"link": "<img src= 'media/mac.jpg'>",
+		"description": "<br>marissa's very famous mac and cheese recipe, she won't share her secret but she says, 'it's bangin'."
 	},
 	{
-		"name": "plant trees",
+		"name": "mac",
 		"type": "image",
-		"link": "media/PlantTrees.jpg",
-		"tag": "roof"
-	},
-	{
-		"name": "rainbow",
-		"type": "image",
-		"link": "media/Rainbow2.jpg",
-		"tag": "roof"
-	},
-	{
-		"name": "bag secured",
-		"type": "image",
-		"link": "media/ACNH.jpg",
-		"tag": "AC"
-	},
-	{
-		"name": "art",
-		"type": "image",
-		"link": "media/WashEm.jpg",
-		"tag": "social media, instagram"
-	},
-	{
-		"name": "twitter",
-		"type": "image",
-		"link": "media/twitter2.png",
-		"tag": "social media, twitter"
+		"link": "<img src= 'media/floorkale.jpg'>",
+		"description": "<br>bonus image: floor kale."
 	},
 	]
 
-	for(var i=0; i<objects.length; i++){
-	$('.object').append('<img class="inserted" src= " ' + objects[i].link + ' ">')
-}
+// document.body.style.backgroundColor = "green";
+		var counter = 0;
+// gives us the mousedown function + creates a class in HTML called mouseDownBox + a variable called photoclick
+		$(window).mousedown(function( event ) {
+			var photoClick = $('<div/>', {
+				'class': 'mouseDownBox'
+			})
+// chooses .pic from the array to add the event listener to
+			.html(foods[counter%foods.length].link)
+			.append(foods[counter%foods.length].description)
+			// .append(photos[counter%photos.length].desc)
+				.hover(function(){
+					$("description").css({'opacity': 100})
+					// $( this ).fadeOut( 100 );
+  			// 		$( this ).fadeIn( 500 );
+				});
+			// appends the variable to the HTML
+			counter = counter+1;
+			$('.food').append(photoClick)
+			});
 
-	for(var i=0; i<objects.length; i++){
-	$('.object').append('<div class="box">' + objects[i].name + '</div>')
-}
 
-$(document).ready(function(){
-  $("#hide").click(function(){
-    $(".object").hide();
-  });
-  $("#show").click(function(){
-    $(".object").show();
-  });
-});
-
-
-
+	
 
