bb5d287b711990acb5959adc29c4ddda0423739b
Sophia Beceiro <sophiabeceiro@Sophias-MacBook-Pro.local>
2020-05-07 01:51:17 -0400

    added recent playlist and fixed my home page

--- labexercise2/main.js
+++ labexercise2/main.js
@@ -7,116 +7,132 @@ var flowers = [
 		"name": "rose",
 		"color": "red",
 		"size": "medium",
-		"location":"asia"
+		"location":"asia",
 		"image":"images/rose.png"
 	},
 	{
 		"name": "daisy",
 		"color": "white",
 		"size": "small",
-		"location":"asia, europe"
+		"location":"asia, europe",
 		"image":"images/daisy.png"
 	},
 	{
 		"name": "sunflower",
 		"color": "yellow",
 		"size": "large",
-		"location":"north america"
+		"location":"north america",
 		"image":"images/sunflower.png"
 	},
 	{
 		"name": "tulip",
 		"color": "pink",
 		"size": "medium",
-		"location":"europe"
+		"location":"europe",
 		"image":"images/tulip.png"
 	},
 	{
 		"name": "lily",
 		"color": "white",
 		"size": "large",
-		"location":"europe, asia, north america"
+		"location":"europe, asia, north america",
 		"image":"images/lily.png"
 	},
 	{
 		"name": "daffodil",
 		"color": "yellow",
 		"size": "medium",
-		"location":"europe"
+		"location":"europe",
 		"image":"images/daffodil.png"
 	},
 	{
 		"name": "orchid",
 		"color": "purple",
 		"size": "large",
-		"location": "south america"
+		"location": "south america",
 		"image":"orchid/rose.png"
 	},
 	{
 		"name": "poppy",
 		"color": "red",
 		"size": "medium",
-		"location": "asia"
+		"location": "asia",
 		"image":"images/poppy.png"
 	},
 	{
 		"name": "pansy",
 		"color": "purple",
 		"size": "small",
-		"location": "europe"
+		"location": "europe",
 		"image":"images/pansy.png"
 	},
 	{
 		"name": "hibiscus",
 		"color": "red",
 		"size": "large",
-		"location": "asia"
-		"image":"images/hibiscus.png"
+		"location": "asia",
+		"image":"img src='images/hibiscus.png'"
 	},
 	{
 		"name": "carnation",
 		"color": "pink",
 		"size": "medium",
-		"location": "europe, asia"
+		"location": "europe, asia",
 		"image":"images/carnation.png"
 	},
 	{
 		"name": "lavender",
 		"color": "purple",
 		"size": "large",
-		"location": "europe, africa"
+		"location": "europe, africa",
 		"image":"images/lavender.png"
 	},
 	{
 		"name": "lilac",
 		"color": "purple",
 		"size": "large",
-		"location": "europe"
+		"location": "europe",
 		"image":"images/lilac.png"
 	},
 	{
 		"name": "violet",
 		"color": "purple",
 		"size": "small",
-		"location": "north america"
+		"location": "north america",
+		"image":"images/violet.png"
 	},
 	{
 		"name": "jasmine",
 		"color": "white",
 		"size": "small",
-		"location": "europe, asia"
+		"location": "europe, asia",
+		"image":"images/jasmine.png"
 	},
 		{
 		"name": "peony",
 		"color": "pink",
 		"size": "large",
-		"location": "europe, asia, north america"
+		"location": "europe, asia, north america",
+		"image":"images/peony.png"
 	}
 ]
 
 for(var i=0; i<flowers.length; i++){
 	$('#flowers').append('<div class="box">' + flowers[i].name + '</div>')
 }
+
+var counter = 0;
+
+		$(window).mousedown(function( event ) {
+			var flowersDiv = $('<div/>', {
+				'class': 'mouseDownBox'
+			})
+			.html(flowers[counter%flowers.length].image)
+			.css({'top': event.pageY-50, 'left': event.pageX-50})
+
+			counter = counter+1;
+			$('body').append(flowersDiv)
+		})
 
 // for(var i=0; i<flowers.length; i++){
 // 	$('#flowers').append(flowers[i].name + "<br>")
